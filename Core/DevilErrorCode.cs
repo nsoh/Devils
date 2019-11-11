a44106d91124d0965de19a3ec830b12e283a9c03
nsoh <hf98066@gmail.com>
2019-11-11 11:15:23 +0900

    주석 추가

--- Core/DevilErrorCode.cs
+++ Core/DevilErrorCode.cs
@@ -2,12 +2,13 @@ using System;
 
 namespace Devils
 {
+    // 에러 코드 정의
     public enum DevilErrorCode : UInt32
     {
         Ok = 0,
 
         ErrorUnknown            = 10000000,
         ErrorTaskNotExists      = 10000001,
-        
+
     }
 }
--- Core/DevilException.cs
+++ Core/DevilException.cs
@@ -2,12 +2,13 @@ using System;
 
 namespace Devils
 {
+    // 예외 처리 클래스
     class DevilException : Exception
     {
         public readonly DevilErrorCode ErrorCode = DevilErrorCode.ErrorUnknown;
 
-        public DevilException(DevilErrorCode errorCode, string _strFormat = "", params object[] _arrObj) 
-            : base(string.Format(_strFormat, _arrObj))
+        public DevilException(DevilErrorCode errorCode, string format = "", params object[] param) 
+            : base(string.Format(format, param))
         {
             ErrorCode = errorCode;
         }
